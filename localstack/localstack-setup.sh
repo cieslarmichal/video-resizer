b384c7c27f05ebce08e08c6369edd7720393ebf9
Michal Cieslar <michalovskyyy@gmail.com>
2023-11-01 23:25:11 +0100

    add uploading sample file to s3 on localstack init

--- localstack/localstack-setup.sh
+++ localstack/localstack-setup.sh
@@ -2,16 +2,16 @@
 
 echo "CREATING S3 BUCKETS"
 
-awslocal s3 mb s3://videos --region eu-central-1
-awslocal s3 mb s3://resized-videos --region eu-central-1
+awslocal s3 mb s3://videos
+awslocal s3 mb s3://resized-videos
 
 echo "S3 BUCKETS CREATED"
 
-awslocal s3 ls --region eu-central-1
+awslocal s3 ls
 
 echo "CREATING S3 OBJECTS"
 
-awslocal s3 cp ./resources/sample_file.mp4 s3://videos/sample_file.mp4
+awslocal s3 cp /usr/share/app/resources/sample_file1.mp4 s3://videos/sample_file1.mp4
 
 awslocal s3 ls s3://videos --recursive --human-readable --summarize
 
