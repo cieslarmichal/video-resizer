be39d893d73c139cccefbe2c994e07be862ca1b9
Michal Cieslar <michalovskyyy@gmail.com>
2023-11-03 10:19:42 +0100

    add queue controllers to lambdas

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,9 @@
+export * from './core/application.js';
+
+export * from './modules/videoModule/symbols.js';
+
+export * from './modules/videoModule/api/queueHandlers/video360pQueueController/video360pQueueController.js';
+
+export * from './modules/videoModule/api/queueHandlers/video480pQueueController/video480pQueueController.js';
+
+export * from './modules/videoModule/api/queueHandlers/video720pQueueHandler/video720pQueueController.js';
--- apps/infrastructure/src/stacks/videoProcessing/lambdas/resizeVideoTo480p/resizeVideoTo480pLambdaHandler.ts
+++ apps/infrastructure/src/stacks/videoProcessing/lambdas/resizeVideoTo480p/resizeVideoTo480pLambdaHandler.ts
@@ -1,15 +1,10 @@
-import { type Handler, type SNSMessage, type S3Event, type SQSEvent } from 'aws-lambda';
+import { Application, type Video480pQueueController, videoSymbols } from '@video-resizer/backend';
+import { type Handler, type SQSEvent } from 'aws-lambda';
+
+const container = Application.createContainer();
+
+const video480pQueueController = container.get<Video480pQueueController>(videoSymbols.video480pQueueController);
 
 export const lambda: Handler = async (sqsEvent: SQSEvent): Promise<void> => {
-  const eventActions = sqsEvent.Records.map(async (sqsRecord) => {
-    const snsMessage = JSON.parse(sqsRecord.body) as SNSMessage;
-
-    const s3Event = JSON.parse(snsMessage.Message) as S3Event;
-
-    const s3EventRecord = s3Event.Records[0];
-
-    console.log(s3EventRecord);
-  });
-
-  await Promise.all(eventActions);
+  await video480pQueueController.handleEvent(sqsEvent);
 };
