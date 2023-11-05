6f04e83ee3c2c95c65a0d094154e76e05314dbeb
Michal Cieslar <michalovskyyy@gmail.com>
2023-11-05 23:04:31 +0100

    fix docker

--- backend/scripts/ffmpeg-setup.sh
+++ backend/scripts/ffmpeg-setup.sh
@@ -2,5 +2,4 @@
 echo 'deb http://www.deb-multimedia.org jessie main non-free' >> /etc/apt/sources.list
 echo 'deb-src http://www.deb-multimedia.org jessie main non-free' >> /etc/apt/sources.list
 apt-get update
-apt-get install -y deb-multimedia-keyring
 apt-get install -y ffmpeg
